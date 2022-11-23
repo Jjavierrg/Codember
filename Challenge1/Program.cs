6628f8458ecc75f54fc1dd8a2296c76ffee099bd
Jose Javier Rodriguez Gallego <jjavierrg@gmail.com>
2022-11-23 09:46:37 +0100

    Add project files.

---
+++ Challenge1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    const string FILENAME = "users.txt";
+
+    private static async Task<List<string>> GetPeopleLines()
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), FILENAME);
+        var fileContent = await File.ReadAllTextAsync(path);
+        var people = fileContent.Split("\n\n", StringSplitOptions.RemoveEmptyEntries).ToList();
+        return people.Select(x => x.Replace("\n", " ").Trim()).ToList();
+    }
+
+    private static bool isValidPerson(string person)
+    {
+        string[] requiredFields = new string[] { "usr", "eme", "psw", "age", "loc", "fll" };
+        return requiredFields.All(x =>
+        {
+            string pattern = @$"\b{x}:";
+            return Regex.IsMatch(person, pattern);
+        });
+
+    }
+
+    private static async Task Main(string[] args)
+    {
+        var people = await GetPeopleLines();
+        var validPeople = people.Where(isValidPerson);
+        var lastPerson = validPeople.LastOrDefault();
+        Console.WriteLine($"{validPeople.Count()} - {lastPerson}");
+    }
+}
---
+++ Challenge2/Program.cs
@@ -0,0 +1,48 @@
+﻿internal class Program
+{
+    const string ENCRYPTED = "11610497110107115 102111114 11210897121105110103 9911110010110998101114 11210810197115101 11510497114101";
+
+    private static bool IsValidAsciiRange(string input)
+    {
+        int val = int.Parse(input);
+        return val >= 65 && val <= 122;
+    }
+
+    private static List<string> SplitLetters(string input)
+    {
+        var result = new List<string>();
+        string word = "";
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            word += input[i];
+            if (IsValidAsciiRange(word))
+            {
+                result.Add(word);
+                word= "";
+            }
+        }
+
+        return result;
+    }
+
+    private static char GetCharFromAsciiCode(string input)
+    {
+        int ascii = int.Parse(input);
+        return Convert.ToChar(ascii);
+
+    }
+
+    private static string DecryptWord(string encryptedWord)
+    {
+        var letters = SplitLetters(encryptedWord).Select(GetCharFromAsciiCode);
+        return string.Join("", letters);
+    }
+
+    private static void Main(string[] args)
+    {
+        var words = ENCRYPTED.Split(' ');
+        var decryptedWords = words.Select(DecryptWord);
+        Console.WriteLine(string.Join(" ", decryptedWords));
+    }
+}
